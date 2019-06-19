36aae36f1167881afc21e8564528c280eea21001
rajesh-ray <rayrajesh31@gmail.com>
2019-06-19 13:21:24 +0530

    Added render if required params missing

--- app/controllers/api/partners_controller.rb
+++ app/controllers/api/partners_controller.rb
@@ -10,21 +10,31 @@ module Api
 			else
 				partner = Partner.order('created_at DESC')
 			end
+			
 			render json: {status: 'SUCCESS', 'message': 'Fetched delivery boys', data: partner}, status: :ok
 		end
 
 		def create
 			partner_params, missing_params = check_and_create_params
 			byebug
+			if missing_params.include? true
+				render json: {status: 'FAILURE', 'message': 'Please provide required params'}, status: :bad_request and return
+			end
+
 			partner = Partner.new(partner_params.with_indifferent_access)
-			partner.save
+			
+			if partner.save
+				render json: {status: 'SUCCESS', 'message': 'Partner created', data: partner}, status: :ok
+			else
+				render json: {status: 'FAILURE', 'message': 'Partner not created', data: partner.errors}, status: :unprocessable_entity
+			end
 		end
 
 		private
 
 		def check_and_create_params
 			required_params = ['name', 'phone', 'coverage']
-			missing_params = required_params.collect{|param| params[params].nil?}
+			missing_params = required_params.collect{|param| params[param].nil?}
 			partner = params.slice(*required_params).as_json
 			partner['coverage'] = set_coordinates_from_lat_long(params['coverage'])
 			partner = partner.slice('name', 'phone', 'coverage')
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -2,22 +2,39 @@ module Api
 	class TasksController < ApplicationController
 		skip_before_action :verify_authenticity_token
 		def index
-			tasks = Task.order('created_at DESC')
-			render json: {status: 'SUCCESS', 'message': 'Fetched delivery tasks', data: tasks}, status: :ok
+			if params[:task_id]
+				task = Task.find(params[:task_id]) rescue nil
+				if task.nil?
+					render json: {status: 'FAILURE', 'message': 'Task id does not exist'}, status: :bad_request and return
+				end
+			else
+				task = Task.order('created_at DESC')
+			end
+
+			render json: {status: 'SUCCESS', 'message': 'Fetched delivery tasks', data: task}, status: :ok
 		end
 
 		def create
 			task_params, missing_params = check_and_create_params
 			byebug
+			if missing_params.include? true
+			 	render json: {status: 'FAILURE', 'message': 'Please provide required params'}, status: :bad_request and return
+			end
+			
 			task = Task.new(task_params.with_indifferent_access)
-			task.save
+			
+			if task.save
+				render json: {status: 'SUCCESS', 'message': 'Task created', data: task}, status: :ok
+			else
+				render json: {status: 'FAILURE', 'message': 'Task not created', data: task.errors}, status: :unprocessable_entity
+			end
 		end
 
 		private
 
 		def check_and_create_params
 			required_params = ['source_lat', 'source_long', 'destination_lat', 'destination_long']
-			missing_params = required_params.collect{|param| params[params].nil?}
+			missing_params = required_params.collect{|param| params[param].nil?}
 			task = params.slice(*required_params).as_json
 			task['source'] = set_coordinates_from_lat_long(params['source_lat'], params['source_long'])
 			task['destination'] = set_coordinates_from_lat_long(params['destination_lat'], params['destination_long'])
